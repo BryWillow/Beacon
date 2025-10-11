a80008d74f24570f31f2363909c34c1597d0a751
BryWillow <168703221+BryWillow@users.noreply.github.com>
2025-10-11 10:53:50 -0500

    Remove build artifacts.

--- build_Debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/bryancamp/SoftwareDevelopment/cpp/professional/Beacon
-# Build directory: /Users/bryancamp/SoftwareDevelopment/cpp/professional/Beacon/build_Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[test_itch_file_generator]=] "/Users/bryancamp/SoftwareDevelopment/cpp/professional/Beacon/build_Debug/test_itch_file_generator")
-set_tests_properties([=[test_itch_file_generator]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/bryancamp/SoftwareDevelopment/cpp/professional/Beacon/CMakeLists.txt;62;add_test;/Users/bryancamp/SoftwareDevelopment/cpp/professional/Beacon/CMakeLists.txt;0;")
-subdirs("vendor/googletest")
